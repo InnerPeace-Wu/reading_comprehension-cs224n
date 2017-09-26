66dbbac2461c521420b322fc1d0492a9b98edf94
innerpeace-wu <innerpeace-wu@outlook.com>
2017-09-26 21:43:47 +0800

    re-new repo

---
+++ code/utils/Config.py
@@ -0,0 +1,7 @@
+# configurations of the model
+
+class Config:
+    # maximum length of context
+    context_max_len = 350
+    # maximum length of question
+    ques_max_len = 25
---
+++ code/utils/visualize_dataset.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from os.path import join as pjoin
+
+ROOT_DIR = '../..'
+Data_dir = 'data/squad'
+# suffixes = ['context', 'question', 'answer']
+suffixes = ['context', 'question']
+set_names = ['train', 'val']
+
+def read_ids(set_name, suffix):
+    file_name = set_name + '.ids.' + suffix
+    file_path = pjoin(ROOT_DIR, Data_dir, file_name)
+    file_path = os.path.abspath(file_path)
+
+    #print(os.path.realpath(__file__))
+    #print(os.path.abspath(__file__))
+
+    assert os.path.exists(file_path), "the file path: {} seems not exist.".format(file_path)
+
+    count_list = []
+    with open(file_path, 'r') as f:
+        # the lines below will read the whole file, that'a bad.
+        # for line in f.readlines():
+        #     count_list.append(len(line.split(' ')))
+        line = f.readline()
+        while line:
+            count_list.append(len(line.split(' ')))
+            line = f.readline()
+
+    return count_list
+    #hist_plot(count_list, set_name, suffix)
+    #print(count_list)
+
+def hist_plot(count_list, suffix, set_name='allset'):
+
+    plt.hist(count_list, normed=True, bins=50)
+    plt.ylabel('Counts')
+    plt.xlabel('Length')
+    plt.title('Histgrams of {}_{}'.format(set_name, suffix))
+    #plt.show()
+
+def draw_hists():
+    l = len(suffixes)
+    fig, _ = plt.subplots(nrows=1, ncols=l)
+    for i in xrange(l):
+        count_list = []
+        for sn in set_names:
+            count_list += read_ids(sn, suffixes[i])
+        plt.subplot(1, l, i+1)
+        hist_plot(count_list, suffixes[i])
+    fig.tight_layout()
+    plt.show()
+    # to gain resolution by set dpi
+    # output_path = 'histgrams-of-dataset.pdf'
+    # plt.savefig(output_path, format='pdf')
+
+
+
+if __name__ == '__main__':
+    #read_ids('val', 'question')
+    draw_hists()
+
