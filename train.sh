daf26c488504e23cc2c79d87638f6dc345ee8795
InnerPeace-Wu <innerpeace.wu@outlook.com>
2017-11-30 15:56:36 +0800

    install requirements

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 apt-get -y update
 apt-get -y install python-tk
+pip install -r requirements.txt
 
 if [ -e "/valohai/inputs/data_squad/data.tar.gz" ]; then
     tar -xvzf /valohai/inputs/data_squad/data.tar.gz
