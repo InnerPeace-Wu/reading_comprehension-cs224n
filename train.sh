8ae34a23da2649bc1427a15abce18f7469be63f0
InnerPeace-Wu <innerpeace.wu@outlook.com>
2017-11-30 09:15:05 +0800

    update train

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install python-tk
+apt-get update
+apt-get install python-tk
 
 if [ -e "/valohai/inputs/data_squad/data.tar.gz" ]; then
     tar -xvzf /valohai/inputs/data_squad/data.tar.gz
